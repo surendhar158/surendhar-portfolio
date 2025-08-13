e12bf5d3619ec0315e54d8f4f5d83b48bb29257b
surendhar@dhya.in <surendhar@dhya.in>
2025-08-13 13:40:06 +0530

    hi

--- src/components/About.js
+++ src/components/About.js
@@ -12,7 +12,7 @@ function About() {
       <div className='flex flex-col justify-center'>
          <h1 className='text-white font-bold text-7xl border-b-4 w-[320px]'> About Me</h1><br/>
 <p className='text-slate-300 text-2xl'>Iam a Front-end  Developer</p><br/>
-<p className='text-2xl font-HERO text-pink-300'> A passionate aspiring frontend developer | react & tailwind enthusiast.
+<p className='text-2xl font-HERO text-pink-300'> A passionate aspiring  developer | react & tailwind enthusiast.
 
 Highly resourceful student eager to contribute to a challenging and competitive environment. Passionate about learning modern web technologies like React and Tailwind CSS to build engaging and intuitive user interfaces. My goal is to continuously enhance my skills and contribute to the personal growth as well as the growth of the organization.</p>
       </div>
